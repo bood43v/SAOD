7f015e002abfce38d5e322f28d4f23c6aef80875
bood43v <budaevgena@gmail.com>
2023-10-31 22:00:51 +0900

    Пример работы с функциями algorithms

---
+++ Projects/TestAlgorithm/TestAlgorithm.cpp
@@ -0,0 +1,69 @@
+﻿// Работа с функциями из библиотеки algorithm
+// @author Будаев Г.Б.
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+void PrintInt(int value)
+{
+    cout << value << " ";
+}
+
+void PrintIntVector(const vector<int>& v)
+{
+    for_each(v.begin(), v.end(), PrintInt);
+}
+
+int main() {
+    //-------- for_each --------//
+    // печать вектора
+    vector<int> v; 
+    for (int i = 1; i < 10; i++)
+    {
+        v.push_back(i);
+    }
+    // так
+    PrintIntVector(v);
+    cout << endl;
+    // или так
+    for_each(v.begin(), v.end(), [](int value) {cout << value << " "; });
+    cout << endl;
+
+
+    //-------- transform --------//
+    // изменение вектора и сохранение результата в другом
+    vector<int> source;
+    for (int i = 0; i < 10; i++)
+    {
+        source.push_back(i);
+    }
+    vector<int> target(source.size());
+    // target.begin - вектор сохранения действий над элементами вектора source (возведение в квадрат
+    transform(source.begin(), source.end(), target.begin(), [](int& a) {return a * a; });
+    // вывод нового вектора
+    for_each(target.begin(), target.end(), [](int a) {cout << a << ' '; });
+
+
+
+    //-------- copy_if --------//
+    // копирование только четных значений вектора
+    vector<int> target2(5);
+    copy_if(source.begin(), source.end(), target2.begin(), [](int value) { return value % 2 == 0; });
+    for_each(target2.begin(), target2.end(), [](int a) {cout << a << ' '; });
+
+
+    //-------- any_of --------//
+    // вывод наличия отрицательных элементов в векторе
+    cout << endl;
+    if (any_of(v.begin(), v.end(), [](int value) { return value < 0; }) == true)
+        cout << "Array has negative element(s)" << endl;
+    else cout << "Array hasn't negative elements" << endl;
+
+
+
+
+
+
+}
--- Projects/Tree/Tree/Tree.cpp
+++ Projects/Tree/Tree/Tree.cpp
@@ -4,22 +4,17 @@
 #include "TreeNode.h"
 #include "BinarySearchTree.h"
 
+#include <algorithm>
 using namespace std;
 
-
-
-
+void summ(int& i, const int value) {
+    i = i + value;
+}
 
 int main()
 {
     /// Создание первого дерева
     BSTree<int>* t1 = new BSTree<int>();
-/*    t1->Insert(5);/// вырожденное 5    
-    t1->Insert(10);///               10
-    t1->Insert(20);///                   20
-    t1->Insert(30);///                       30    
-    t1->Insert(40);///                          40*/
-
     t1->Insert(10);//          10  
     t1->Insert(5);//       5        15
     t1->Insert(2);///  2     6   12
@@ -43,8 +38,6 @@ int main()
     BSTree<int>* t3;
     t3 = t2;
 
-    //t3->Remove(5);
-
     int* arr3 = new int[t3->Size()];
     t3->AddToArrayLNR(arr3);
     cout << "t3: ";
@@ -54,24 +47,13 @@ int main()
     }
 
 
-    ///// Создание четвертого и использование apply для функции double_
-    //BSTree<int>* t4 = new BSTree<int>();
-    //t4 = t3;
-    //apply(t4->Root(), double_);
-
-    //int i = 0;
-    //int* arr4 = new int[t4->Size()];
-    //t4->AddToArrayLNR(arr4);
-    //cout << endl << "t4: ";
-    //for (int i = 0; i < t4->Size(); i++)
-    //{
-    //    cout << arr4[i] << " ";
-    //}
     cout << endl << endl << "t2 used iterator: " << endl;
     for (BSTree<int>::Iterator<int> it = t2->Begin(); it != t2->End(); ++it) {
         int value = *it;
         cout << value << " ";
     }
 
+
+ 
 }
 
